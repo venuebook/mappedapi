d496037b182a52dd4c9b77d5fae25e6f807fd216
Dan Pozmanter <dan.pozmanter@venuebook.com>
2016-05-16 15:53:31 -0400

    Adding tests

---
+++ mappedapi/tests/test_base.py
@@ -0,0 +1,129 @@
+import json
+import pytest
+import responses
+import mappedapi.base
+
+"""
+Test basic functionality of MappedAPI with an example implementation.
+"""
+
+########################
+# TEST DATA
+########################
+
+BASE_URL = 'http://localhost'
+
+TEST_DOG_ID = '1'
+TEST_DOG = {
+    'display_name': 'Doctor Woof',
+    'bark': 'loud',
+    'doge': 'wow',
+}
+
+########################
+# TEST IMPLEMENTATION
+########################
+
+RESOURCE_MAPPING = {
+    'dogs': {
+        'shibes': {
+            'get': {
+                'endpoint_base': ['dogs', 'shibes'],
+                'ids': ['dog_id'],
+                'verb': 'get',
+            },
+            'post': {
+                'endpoint_base': ['dogs', 'shibes'],
+                'ids': ['dog_id'],
+                'verb': 'post',
+            },
+        },
+    },
+}
+
+class APIResourceItem(mappedapi.base.APIResourceItem):
+    """Item in a APIResource - Either a nested resource or an action."""
+
+    def _get_base_url(self):
+        return BASE_URL
+
+    def _get_headers(self):
+        return {
+            'Authorization': 'Bearer %s' % self.auth['token'],
+        }
+
+class APIResource(mappedapi.base.APIResource):
+    """Top Level API Resource"""
+
+    ITEM_CLASS = APIResourceItem
+    RESOURCE_MAPPING = RESOURCE_MAPPING
+
+class Client(mappedapi.base.Client):
+    RESOURCE_CLASS = APIResource
+    def __init__(self, access_token):
+        self.auth = {'token': access_token}
+
+########################
+# TESTS
+########################
+
+@pytest.fixture(scope="module")
+def client():
+    """Setup RequestHandler instance with test values."""
+    return Client('Token')
+
+def test_getattr_error_api_resource():
+    """Test getting an attribute that doesn't exist."""
+    with pytest.raises(AttributeError) as exception_info:
+        client().dogs.wow
+    assert(exception_info.typename == 'AttributeError')
+
+def test_getattr_error_api_resource_item():
+    """Test getting an attribute that doesn't exist."""
+    with pytest.raises(AttributeError) as exception_info:
+        client().dogs.shibes.wow
+    assert(exception_info.typename == 'AttributeError')
+
+def test_getattr_error_client():
+    """Test getting an attribute that doesn't exist."""
+    with pytest.raises(AttributeError) as exception_info:
+        client().wow
+    assert(exception_info.typename == 'AttributeError')
+
+@responses.activate
+def test_error():
+    """Test client.users.get"""
+    responses.add(method=responses.GET,
+        url='%s/dogs/%s/shibes' % (BASE_URL, TEST_DOG_ID),
+        body=json.dumps(TEST_DOG),
+        status=400,
+        content_type='application/json'
+        )
+    with pytest.raises(Exception) as exception_info:
+        r = client().dogs.shibes.get(dog_id=TEST_DOG_ID)
+    assert exception_info.typename == 'MappedAPIRequestError'
+
+@responses.activate
+def test_endpoint_get():
+    """Test client.users.badge.get"""
+    print('%s/dogs/%s/shibes' % (BASE_URL, TEST_DOG_ID))
+    responses.add(method=responses.GET,
+        url='%s/dogs/%s/shibes' % (BASE_URL, TEST_DOG_ID),
+        body=json.dumps(TEST_DOG),
+        status=200,
+        content_type='application/json'
+        )
+    r = client().dogs.shibes.get(dog_id=TEST_DOG_ID)
+    assert r.status_code == 200
+
+@responses.activate
+def test_endpoint_post():
+    """Test client.users.create"""
+    responses.add(method=responses.POST,
+        url='%s/dogs/%s/shibes' % (BASE_URL, TEST_DOG_ID),
+        body=json.dumps(TEST_DOG),
+        status=201,
+        content_type='application/json'
+        )
+    r = client().dogs.shibes.post(dog_id=TEST_DOG_ID, data=TEST_DOG)
+    assert r.status_code == 201
